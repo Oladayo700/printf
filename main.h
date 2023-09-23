1a605d0f964e6cc47991719ea41e109940eeedfd
Oladayo700 <olanipekunoladayo400@gmail.com>
2023-09-23 12:44:15 -0700

    Task 1 correction

--- main.h
+++ main.h
@@ -1,12 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdarg.h>
+#include <stdio.h>
 
 /* Function prototype for custom _printf function */
 int _printf(const char *format, ...);
 
 /* Function prototype for the custom_printf_binary function */
 int custom_printf_binary(const char *format, ...);
+
+/**
+ * handle_conv - Custom printf-like function for 'd' and 'i'
+ * @format: The format string
+ * @...: Variable number of arguments
+ */
+void handle_conv(const char *format, ...);
 
 #endif /* MAIN_H */
